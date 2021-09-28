c77591d3f23e1dea5050e913530dc67997338c8f
AJ <aj@smartnvm.com>
2021-09-28 00:41:24 +0000

    add random letter generator, update urlDatabase with generated shortURL and longURL in the form user input field

--- express_server.js
+++ express_server.js
@@ -4,6 +4,27 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const generateRandomString = (length) => {
+  let shortURL = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$^()-+][><?~=';
+  let charLength = characters.length
+  for (i = 0; i < length; i++) {
+
+    // let num = Math.floor((Math.random() * 127))
+    // shortURL += String.fromCharCode(num)
+
+    let num = Math.floor((Math.random() * charLength))
+    shortURL += characters[num]
+  }
+  return shortURL
+}
+
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -31,10 +52,15 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-//  console.log(templateVars)
+  console.log(templateVars)
   res.render("urls_index", templateVars);
 });
 
+
+app.get("/urls/new", (req, res) => {
+  console.log(req.params)
+  res.render("urls_new");
+});
 
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -42,9 +68,27 @@ app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL
 
   const templateVars = {
-     shortURL: shortURL,
-     longURL: urlDatabase[shortURL]
-    };
-  //console.log(templateVars)
+    shortURL: shortURL,
+    longURL: urlDatabase[shortURL]
+  };
+  console.log(templateVars)
   res.render("urls_show", templateVars);
 });
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+
+
+  const longURL = req.body.longURL
+  const shortURL = generateRandomString(6)
+  
+  urlDatabase[shortURL] = longURL
+
+  console.log(urlDatabase)
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
+
+
