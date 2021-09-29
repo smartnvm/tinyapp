06c66a0053436ada1634612cc5f90f356ae2ecbc
AJ <aj@smartnvm.com>
2021-09-29 05:28:08 +0000

    add logic to check for existing longURL before adding, add login/logout end points with cookies, move application logic to external ./src/appFn.js file

--- express_server.js
+++ express_server.js
@@ -1,3 +1,5 @@
+
+
 const express = require("express");
 const app = express();
 const PORT = 8080;
@@ -7,42 +9,10 @@ app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const cookies = require("cookie-parser");
+app.use(cookies());
 
-const generateRandomString = (length) => {
-  let shortURL = '';
-  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@$^()-+][><~=';
-  let charLength = characters.length
-  for (i = 0; i < length; i++) {
-
-    // let num = Math.floor((Math.random() * 127))
-    // shortURL += String.fromCharCode(num)
-
-    let num = Math.floor((Math.random() * charLength))
-    shortURL += characters[num]
-  }
-  return shortURL
-}
-
-const validateURL = (str) => {
-  const pattern = new RegExp('^(http?:\\/\\/)?' + // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-    '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
-
-  const pattern2 = new RegExp('^(https?:\\/\\/)?' + // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-    '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
-
-  // console.log('http: ',pattern.test(str))
-  // console.log('https:',pattern2.test(str))
-  return !!pattern.test(str) || !!pattern2.test(str);
-}
-
+const { generateRandomString, validateURL, getKeyByValue } = require('./src/appFn')
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -62,18 +32,25 @@ app.get("/", (req, res) => {
 
 //main app view "urls_index"
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  //console.log(templateVars)
+  const templateVars = {
+    errCode: 300,
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
+  console.log(templateVars)
   res.render("urls_index", templateVars);
 });
 
 
 app.get("/urls/new", (req, res) => {
-  const errCode = 200
-  console.log({ errCode })
-  res.render('urls_new', { errCode })
-});
+  const templateVars = {
+    errCode: 200,
+    username: req.cookies["username"],
+    urls: urlDatabase
+  }
 
+  res.render('urls_new', templateVars)
+});
 
 //found
 app.get("/u/:shortURL", (req, res) => {
@@ -84,21 +61,92 @@ app.get("/u/:shortURL", (req, res) => {
 
 //retrieve user input 
 app.post("/urls", (req, res) => {
+  
+  username = req.cookies["username"]
+  
   const longURL = req.body.longURL
   console.log(req.body)
+
+  const urlExist = getKeyByValue(urlDatabase, longURL)
+
+  if (urlExist) {
+    const templateVars = {
+      errCode: 410,
+      username: username,
+      urls: urlDatabase
+    }
+    //console.log({ errCode })
+    res.render('urls_new', templateVars)
+    return
+
+  }
+
   //return
   if (!validateURL(longURL)) {
-    errCode = 406
-    console.log({ errCode })
-    res.render('urls_new', { errCode })
+    const templateVars = {
+      errCode: 406,
+      username: username,
+      urls: urlDatabase
+    }
+    //console.log({ errCode })
+    res.render('urls_new', templateVars)
     return
   }
+
+
   const shortURL = generateRandomString(6)
   urlDatabase[shortURL] = longURL
-  res.redirect("/")
 
+  const templateVars = {
+    errCode: 200,
+    username: username,
+    urls: urlDatabase
+  };
+  res.render("urls_index", templateVars)
 });
 
+app.post("/login", (req, res) => {
+
+  const username = req.body.username
+
+  if (username.length < 6) {
+    const templateVars = {
+      errCode: 400,
+      urls: urlDatabase,
+      username: undefined
+    }
+    console.log({ templateVars })
+    res.render('urls_index', templateVars)
+    return
+  }
+
+  res.cookie('username', username)
+
+  const templateVars = {
+    errCode: 200,
+    urls: urlDatabase,
+    username: username
+  }
+
+  console.log(templateVars)
+  res.render("urls_index", templateVars);
+  return
+
+  // res.redirect("/")
+  return
+})
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username", req.body.username);
+  const templateVars = {
+    errCode: 200,
+    username: undefined, //req.cookies["username"]
+    urls: urlDatabase
+  }
+
+  res.render("urls_index", templateVars);
+  return
+})
 
 
 app.post("/urls/:shortURL/delete", (req, res) => {
@@ -114,28 +162,25 @@ app.get("/urls/:shortURL", (req, res) => {
   //parse anything after : 
   const shortURL = req.params.shortURL
 
-  if (!urlDatabase.hasOwnProperty(shortURL)) {
-    res.redirect('/error')
-
+  if (!urlDatabase[shortURL]) {
+    res.redirect('/404')
+    return
   }
 
   const templateVars = {
     shortURL: shortURL,
     longURL: urlDatabase[shortURL],
-    errCode : 200
+    errCode: 200,
+    username: req.cookies["username"]
   };
 
-  
   console.log(templateVars)
-  oldKey = shortURL;
 
   res.render("urls_show", templateVars);
 });
 
 
-const getKeyByValue = (object, value) => {
-  return Object.keys(object).find(key => object[key] === value);
-}
+
 
 //retrieve user input 
 app.post("/urls/:shortURL", (req, res) => {
@@ -144,14 +189,15 @@ app.post("/urls/:shortURL", (req, res) => {
   const oldKey = req.params.shortURL
 
   console.log('oldKey:', oldKey, "new:", newURL)
-
+  oldKey
   if (!validateURL(newURL)) {
     const templateVars = {
       shortURL: oldKey,
       longURL: urlDatabase[oldKey],
-      errCode : 406
+      errCode: 406,
+      username: req.cookies["username"]
     };
-  
+
     console.log(templateVars)
     res.render("urls_show", templateVars);
     return
@@ -163,9 +209,14 @@ app.post("/urls/:shortURL", (req, res) => {
 })
 
 
+
 //main app view "urls_index"
 app.get("/:random", (req, res) => {
-  const templateVars = { errCode: 404 }
+
+  const templateVars = {
+    errCode: 404,
+    username: req.cookies["username"]
+  }
 
   console.log(templateVars)
   res.render("err_page", templateVars);
